84031dbd92d825c908867cb58e20dfafdb89475e
Ben <benjaminravioli@gmail.com>
2023-07-07 11:44:30 -0400

    Created translator and tests

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,15 @@
+"""
+Translates from French to English and vice versa
+"""
+from deep_translator import MyMemoryTranslator
+
+def english_to_french(english_text):
+    french_text = MyMemoryTranslator(source='auto', target='french').translate(english_text)
+
+    return french_text
+
+
+def french_to_english(french_text):
+    english_text = MyMemoryTranslator(source='auto', target='english').translate(french_text)
+
+    return english_text
